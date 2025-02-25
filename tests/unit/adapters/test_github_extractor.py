c4f13be4c7db1c30be9e509c73a35bd11d570cdb
duprijil <d.prijilevschi@gmail.com>
2025-02-25 21:37:55 +0200

    add more coverage on github extractor

--- tests/unit/adapters/test_github_extractor.py
+++ tests/unit/adapters/test_github_extractor.py
@@ -2,6 +2,7 @@ import shutil
 import tempfile
 from pathlib import Path
 
+import pytest
 from git import Repo
 
 from mapping_suite_sdk.adapters.extractor import GithubPackageExtractor
@@ -30,9 +31,9 @@ def test_github_extractor_success_with_default_args(dummy_github_project_path: P
         assert any(result_path.iterdir())
 
 
-def test_github_extractor_success_with_branch_args_and_cleanup(dummy_github_project_path: Path,
-                                                               dummy_github_branch_name: str,
-                                                               dummy_packages_path_pattern: str) -> None:
+def test_github_extract_temporary_success_with_branch_args_and_cleanup(dummy_github_project_path: Path,
+                                                                       dummy_github_branch_name: str,
+                                                                       dummy_packages_path_pattern: str) -> None:
     # Setup
     with tempfile.TemporaryDirectory() as tmp_dir:
         repo_path = Path(tmp_dir) / dummy_github_project_path.name
@@ -52,3 +53,48 @@ def test_github_extractor_success_with_branch_args_and_cleanup(dummy_github_proj
 
         for package_path in packages_path:
             assert not package_path.exists()
+
+
+def test_github_extract_temporary_fails_on_wrong_project_path(dummy_github_branch_name: str,
+                                                              dummy_packages_path_pattern: str) -> None:
+    with pytest.raises(ValueError):
+        with GithubPackageExtractor().extract_temporary(repository_url="wrong/url",
+                                                        packages_path_pattern=dummy_packages_path_pattern,
+                                                        branch_or_tag_name=dummy_github_branch_name):
+            pass
+
+
+def test_github_extract_temporary_fails_on_bad_pattern(dummy_github_project_path: Path,
+                                                       dummy_github_branch_name: str) -> None:
+    with pytest.raises(ValueError):
+        with GithubPackageExtractor().extract_temporary(repository_url=dummy_github_project_path,
+                                                        packages_path_pattern="wrong*_pattern",
+                                                        branch_or_tag_name=dummy_github_branch_name):
+            pass
+
+
+def test_github_extract_temporary_fails_on_wrong_tag_name(dummy_github_project_path: Path,
+                                                          dummy_packages_path_pattern: str) -> None:
+    with pytest.raises(ValueError):
+        with GithubPackageExtractor().extract_temporary(repository_url=dummy_github_project_path,
+                                                        packages_path_pattern=dummy_packages_path_pattern,
+                                                        branch_or_tag_name="wrong_branch"):
+            pass
+
+
+def test_github_extract_temporary_fails_on_empty_project_path(dummy_github_branch_name: str,
+                                                              dummy_packages_path_pattern: str) -> None:
+    with pytest.raises(ValueError):
+        with GithubPackageExtractor().extract_temporary(repository_url="",
+                                                        packages_path_pattern=dummy_packages_path_pattern,
+                                                        branch_or_tag_name=dummy_github_branch_name):
+            pass
+
+
+def test_github_extract_temporary_fails_on_empty_pattern(dummy_github_project_path: Path,
+                                                       dummy_github_branch_name: str) -> None:
+    with pytest.raises(ValueError):
+        with GithubPackageExtractor().extract_temporary(repository_url=dummy_github_project_path,
+                                                        packages_path_pattern="",
+                                                        branch_or_tag_name=dummy_github_branch_name):
+            pass

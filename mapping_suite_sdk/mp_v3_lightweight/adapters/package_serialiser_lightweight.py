28398e506d573a2272bcff7a5bab58254bf09cf2
Alexandros Vassiliades <valexande@csd.auth.gr>
2025-11-09 03:10:04 +0200

    feat: Define MPv3 lightweight serializer

---
+++ mapping_suite_sdk/mp_v3_lightweight/adapters/package_serialiser_lightweight.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+from mapping_suite_sdk.core.adapters.serialiser import MappingPackageAssetSerialiser, \
+    TechnicalMappingCollectionAssetSerialiser, VocabularyMappingCollectionAssetSerialiser
+from mapping_suite_sdk.core.adapters.tracer import traced_class
+from mapping_suite_sdk.mp_v3_lightweight.adapters.mp_v3_serialiser_lightweight import MappingPackageV3LightweightMetadataSerialiser
+from mapping_suite_sdk.mp_v3_lightweight.models.mapping_package_v3_lightweight import MappingPackageV3Lightweight
+
+
+@traced_class
+class MappingPackageV3LightweightSerialiser(MappingPackageAssetSerialiser):
+    """Main serialiser for complete mapping packages."""
+
+    def serialise(self, package_folder_path: Path, asset: MappingPackageV3Lightweight) -> None:
+        """Serialize all components of a mapping package.
+
+        This method orchestrates the serialization of:
+        - Package metadata
+        - Technical mapping suite
+        - Vocabulary mapping suite
+
+        Args:
+            package_folder_path (Path): Path to the mapping package folder.
+            asset (MappingPackageABC): Complete mapping package to serialize.
+        """
+
+        # Serialize each component
+        MappingPackageV3LightweightMetadataSerialiser().serialise(package_folder_path, asset.metadata)
+        TechnicalMappingCollectionAssetSerialiser().serialise(package_folder_path, asset.technical_mapping_suite)
+        VocabularyMappingCollectionAssetSerialiser().serialise(package_folder_path, asset.vocabulary_mapping_suite)
